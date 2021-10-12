f7096e1d183deb1e6c561f69d79c7899d7ef1951
SeungMin <lsm01279@naver.com>
2021-10-12 23:42:53 +0900

    스크립트
    셀렉트 신과 스타트신에서 사용한 스크립트추가

---
+++ Assets/script/StartScene/ButtonSystem.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+public class ButtonSystem : MonoBehaviour
+{
+    [SerializeField]
+    AudioClip[] buttonSound;
+
+    int ButtonTime = 0;
+
+    AudioSource bsSource;
+
+    private bool buttonAble = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        bsSource = GetComponent<AudioSource>();
+    }
+
+    private void ButtonSound_Play()
+    {
+        bsSource.clip = buttonSound[0];
+        bsSource.Play();
+    }
+
+    public void NextScene()
+    {
+        if (buttonAble == true)
+        {
+            ButtonSound_Play();
+            SceneManager.LoadScene("StoryScene");
+        }
+    }
+
+    public void QuitGame()
+    {
+        ButtonSound_Play();
+        if (buttonAble == true)
+        {
+#if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit(); 
+#endif
+        }
+    }
+
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (SpriteManager.spriteCheck == true)
+        {
+            buttonAble = true;
+        }
+
+
+    }
+}
---
+++ Assets/script/StartScene/SpriteManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SpriteManager : MonoBehaviour
+{
+
+    public static bool spriteCheck = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject spImage = this.gameObject;
+        Color color = spImage.GetComponent<Image>().color;
+        color.a = 0;
+        spImage.GetComponent<Image>().color = color;
+    }
+
+    private void SpritePlus()
+    {
+        GameObject spImage = this.gameObject;
+        Color color = spImage.GetComponent<Image>().color;
+        color.a += Time.deltaTime * 0.5f;
+        spImage.GetComponent<Image>().color = color;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (SoundManager.crowCheck == true)
+        {
+            if (this.gameObject.name.Contains("Logo"))
+            {
+                SpritePlus();
+            }
+
+        }
+
+        if(SoundManager.bgmusicCheck==true)
+        {
+            if(this.gameObject.name.Contains("Button"))
+            {
+                SpritePlus();
+                spriteCheck = true;
+            }
+        }
+
+
+
+    }
+}
