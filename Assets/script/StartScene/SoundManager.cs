59bfd21cdb20e819ee9efe7728a132185a45fa05
WhiteWeb0707 <91812936+WhiteWeb0707@users.noreply.github.com>
2021-11-02 15:06:48 +0900

    2

--- Assets/script/StartScene/SoundManager.cs
+++ Assets/script/StartScene/SoundManager.cs
@@ -5,44 +5,27 @@ using UnityEngine;
 
 public class SoundManager : MonoBehaviour
 {
-    [SerializeField]
-    AudioClip[] mainSound;
+    public AudioClip StartBgm;
 
     AudioSource soundSource;
 
-    public static bool crowCheck = false;
     public static bool bgmusicCheck = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        soundSource = GetComponent<AudioSource>();
-        StartCoroutine("PlayList");
+        this.soundSource = GetComponent<AudioSource>();
     }
 
 
-    IEnumerator PlayList()
-    {
-        soundSource.clip = mainSound[0];
-        soundSource.Play();
-        crowCheck = true;
-        while (true)
-        {
-            yield return new WaitForSeconds(1.0f);
-            if (!soundSource.isPlaying)
-            {
-                soundSource.clip = mainSound[1];
-                soundSource.Play();
-                soundSource.loop = true;
-                bgmusicCheck = true;
-            }
-        }
-    }
-
+    
 
     // Update is called once per frame
     void Update()
     {
-        
+        soundSource.clip = StartBgm;
+        soundSource.Play();
+        soundSource.loop = true;
+        bgmusicCheck = true;
     }
 }
